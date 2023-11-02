09b28ff9b1bd56c39b2b63032991aa3d35fd9a01
Nefee1 <katalitika@mail.ru>
2023-11-02 16:06:48 +0700

    add Hogwarts

---
+++ src/Hogwarts.java
@@ -0,0 +1,44 @@
+public class Hogwarts {
+    protected String name;
+    protected int magicalTransgression;
+    protected int powerOfMagic;
+
+    public Hogwarts(String name, int magicalTransgression, int powerOfMagic) {
+        this.name = name;
+        this.magicalTransgression = magicalTransgression;
+        this.powerOfMagic = powerOfMagic;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMagicalTransgression() {
+        return magicalTransgression;
+    }
+
+    public int getPowerOfMagic() {
+        return powerOfMagic;
+    }
+
+    @Override
+    public String toString() {
+        return "Hogwarts{" +
+                "name='" + name + '\'' +
+                ", magicalTransgression=" + magicalTransgression +
+                ", powerOfMagic=" + powerOfMagic +
+                '}';
+    }
+
+    public void comparison(Hogwarts hogwarts){
+        if (powerOfMagic + magicalTransgression > hogwarts.getMagicalTransgression()  + hogwarts.getPowerOfMagic()){
+            System.out.println(name + " сильнее " + hogwarts.getName());
+        }else {
+            System.out.println(hogwarts.getName() + " сильнее " + name);
+
+        }
+    }
+}
+
+
+
